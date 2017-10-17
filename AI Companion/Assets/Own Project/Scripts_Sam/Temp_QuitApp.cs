19ce9e3b23affb52a4f233b9564d787fa2afa289
samvermeir <sam.vermeir@student.howest.be>
2017-10-17 16:39:20 +0200

    first commit

---
+++ AI Companion/Assets/Own Project/Scripts_Sam/Temp_QuitApp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Temp_QuitApp : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKey(KeyCode.P))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
